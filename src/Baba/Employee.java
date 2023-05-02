3624f6701c19d4e14f18aec1ffee45b0814e5f62
Joyston D'Souza <joydsouza140@gamil.com>
2023-05-02 16:47:12 +0530

    commit1.....

---
+++ src/Baba/Employee.java
@@ -0,0 +1,34 @@
+package Baba;
+
+// fc goa
+public class Employee {
+
+	int employeeId;
+	String employeeName;
+	
+	void setEmployeeDetails(int id,String name)
+	{
+		
+		employeeId=id;
+		employeeName=name;
+	}
+	int getEmployeeId()
+	{
+		return employeeId;
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Employee e1=new Employee();
+		e1.setEmployeeDetails(23, "John");
+		
+		System.out.println(e1.getEmployeeId());
+		System.out.println(e1.getEmployeeName());
+		
+		
+
+	}
+
+}
